0db856e7415f7ba1030fbbb53ad056d4012f88b9
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-17 11:13:16 +0900

    Dicee

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    //뷰가 로드될때 처음 실행되는 코드
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Who.What = Value
+        //diceImageView1의 주사위를 6으로 변경
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // imageliteral
+        //imageView의 투명도를 0.5로
+        diceImageView1.alpha = 0.5
     }
 
 
