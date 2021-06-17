be17f87ed9689302354e3aef502130ee538db1d0
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-17 12:00:50 +0900

    Dicee 4

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,23 +12,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    //변수 생성
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
     
-   
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
       
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        //디버그 콘솔에 출력할 내용을 적는 코드
-        print("Button got tapped")
-        //button을 누르면 주사위가 4가 되도록 함.
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        //[]은 컬렉션 모음을 뜻함 배열은 0부터 시작함.
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        //롤 버튼을 누를때, leftDiceNumber의 숫자가 1씩 늘어남
+        leftDiceNumber = leftDiceNumber + 1
+        //롤 버튼을 누를때, rightDiceNumber의 숫자가 1씩 줄어듬
+        rightDiceNumber = rightDiceNumber - 1
     }
 }
 
